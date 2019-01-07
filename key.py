9933cdf9a96525662c600326976b94b6cdf95d75
4seaday <45973483+4seaday@users.noreply.github.com>
2019-01-08 01:28:56 +0900

    Delete key.py

--- key.py
+++
@@ -1,6 +0,0 @@
-class key:
-    def __init__(self):
-        self.user = 'comon'
-        self.passwd = '306crewcodymonster'
-        self.host = 'codymonster-maria.cfnceagzudnn.ap-northeast-2.rds.amazonaws.com'
-        self.port = 3306
